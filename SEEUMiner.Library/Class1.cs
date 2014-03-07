382b074f77ab45c3e08883e922ec4fb4d7f033e1
vshehu <vshehu@gmail.com>
2014-03-07 12:57:04 +0100

    initial commit
    initial commit (visarshehu)

---
+++ SEEUMiner.Library/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SEEUMiner.Library
+{
+    public class Class1
+    {
+        //CTOR
+        public Class1()
+        {
+
+        }
+    }
+}
